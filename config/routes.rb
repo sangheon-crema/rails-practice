0a50c1a5ef8448a489d3529c0670a2764687cefa
sangheon-crema <sangheon.lee@cre.ma>
2023-02-14 15:11:13 +0900

    creating article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
   get "/articles/:id", to: "articles#show"
+  post "/articles", to: "articles#create"
 end
